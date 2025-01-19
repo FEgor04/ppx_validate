a1fc531b3e4641a3f8d53f4ec085a928c77b3875
Egor Fedorov <fegor2004@gmail.com>
2025-01-19 21:10:07 +0300

    fix(bin): add polymorphic show_error function

--- bin/main.ml
+++ bin/main.ml
@@ -2,5 +2,10 @@ type t = { x : string; [@min_length 4] [@max_length 8] y : int; z : string }
 [@@deriving validate, show]
 
 let value = { x = "12345"; y = 123; z = "123" }
-let show_result = function Ok ok -> show ok | Error err -> err
+let show_error = function
+  | `X err -> "x: " ^ err
+  | `Y err -> "y: " ^ err
+  | `Z err -> "z: " ^ err
+
+let show_result = function Ok ok -> show ok | Error err -> show_error err
 let () = validate_t value |> show_result |> print_endline
